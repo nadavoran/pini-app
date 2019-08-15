4b6e9c868fdfb4469f3dd7d2a0a7a69bda4c7e70
Nadav Oran <nadavoran@gmail.com>
2019-08-15 14:12:25 +1000

    Updates

---
+++ precache-manifest.2438f2b755bde472a415c83ea718a25c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff2e94cca37e5bb5e7e79b40bd853cd",
+    "url": "/pini-app/index.html"
+  },
+  {
+    "revision": "787885afaa5a150c7297",
+    "url": "/pini-app/static/css/main.fa48b8be.chunk.css"
+  },
+  {
+    "revision": "2e25fd68c4bd0958e92b",
+    "url": "/pini-app/static/js/2.5b20e6db.chunk.js"
+  },
+  {
+    "revision": "787885afaa5a150c7297",
+    "url": "/pini-app/static/js/main.45203885.chunk.js"
+  },
+  {
+    "revision": "922437a4d392d9559de5",
+    "url": "/pini-app/static/js/runtime~main.08593c3f.js"
+  }
+]);
